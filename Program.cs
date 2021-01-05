d86b6cb8cf3104b64c9b65d1844c3634719cb737
Koelbe <76868849+Koelbe@users.noreply.github.com>
2021-01-05 02:28:11 -0500

    Added project files.

---
+++ Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace FileEncryptor
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select the file you would like to encrypt or decrypt.");
+            Console.Write("Press any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+
+            OpenFileDialog fileDialog = new OpenFileDialog();
+
+            if(fileDialog.ShowDialog() == DialogResult.OK) 
+            {
+                var fs = fileDialog.OpenFile();
+                StreamReader sr = new StreamReader(fs);
+                Console.WriteLine("File Selected: {0}",fileDialog.FileName);
+                Console.WriteLine();
+                string input = "";
+                while (true) 
+                {
+                    Console.Write("Would you like to encrypt/decrypt: ");
+                    input = Console.ReadLine();
+                    input.ToLower();
+                    if(input == "encrypt") { break; }
+                    if(input == "decrypt") { break; }
+                }
+
+                if (input == "encrypt")
+                {
+                    Console.WriteLine("You chose ENCRYPT");
+                    Console.Write("Encryption password: ");
+                    string encryptionPassword = Console.ReadLine();
+                }
+
+                if (input == "decrypt")
+                {
+                    Console.WriteLine("You chose DECRYPT");
+                    Console.Write("Decryption password: ");
+                    string decryptionPassword = Console.ReadLine();
+                }
+
+                Console.ReadKey();
+            }
+
+
+        }
+    }
+}
